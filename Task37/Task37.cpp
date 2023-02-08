08698a2ce84afb089188f8becb60c8c42050e8f0
Chizhikov_IP <Chizhikov_IP@directum.ru>
2023-02-08 17:22:39 +0400

    Задание 3-7

---
+++ Task37/Task37.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+    int sum = 0;
+    for (int i = 1; i < 101; i++)
+    {
+        sum = sum + i;
+    }
+    printf("%i\n", sum);  
+}
+
+//https://github.com/Trivin/LMS.git

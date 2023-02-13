c983cd6b8f0a8608815855fd7ee7a3f521e558c3
Chizhikov_IP <Chizhikov_IP@directum.ru>
2023-02-13 17:53:12 +0400

    Задание 5-2

---
+++ Task52/Task52.cpp
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+int main()
+{
+    int numbers[100][100];
+    int rows;
+    int columns;
+    printf("Array rows(<100): ");
+    scanf("%i", &rows);
+    printf("Array columns(<100): ");
+    scanf("%i", &columns);
+
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+    {
+        printf("Number %i - %i : ", i+1, j+1);
+        scanf("%i", &numbers[i][j]);
+    }
+    }
+    printf("\n");
+
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            printf("%i", numbers[i][j]);
+        }
+        printf("\n");
+    }
+}
+
+//https://github.com/Trivin/LMS.git

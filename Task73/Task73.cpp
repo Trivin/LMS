31f126b4b98c5b9144953300477da38a4e592fba
Chizhikov_IP <Chizhikov_IP@directum.ru>
2023-02-14 20:55:29 +0400

    Задание 7-3

---
+++ Task73/Task73.cpp
@@ -0,0 +1,68 @@
+#include <stdio.h>
+
+int new_sqrt(int num)
+{
+    for (int i = 1; i < num; i++)
+    {
+        if (i*i == num)
+            return i;
+    }
+    return 0;
+}
+
+void new_sqrt(int num, int *sqrt)
+{
+    *sqrt = 0;
+    for (int i = 1; i < num; i++)
+    {
+        if (i*i == num)
+        {
+            *sqrt = i;
+            return;
+        }
+        
+    }
+    return;
+}
+
+void new_sqrt(int num, int &sqrt)
+{
+    sqrt = 0;
+    for (int i = 1; i < num; i++)
+    {
+        if (i*i == num)
+        {
+            sqrt = i;
+            return;
+        }
+    }
+    return;
+}
+
+int main()
+{
+    int num[3];
+    int sqrt[3];
+
+    for (int i = 0; i < 3; i++)
+    {
+        printf("Number1: ");
+        scanf("%i", &num[i]);  
+    }
+
+    sqrt[0] = new_sqrt(num[0]);
+    new_sqrt(num[1], &sqrt[1]);
+    new_sqrt(num[2], sqrt[2]);
+    
+    for (int i = 0; i < 3; i++)
+    {
+        if (sqrt[i] > 0)
+            printf("%i is square of %i\n", num[i], sqrt[i]);
+        else
+            printf("%i not a square\n", num[i]);
+    }
+}
+
+
+
+//https://github.com/Trivin/LMS.git
